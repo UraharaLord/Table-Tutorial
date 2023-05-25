58cb40c3709d80e867641a328c95d378d9d28591
Uriel Rodriguez Sandoval <uriel.rodriguez@gonet.us>
2023-05-25 11:45:44 -0600

    fix

---
+++ ghmjolnircore.xcframework/ios-arm64_x86_64-simulator/ghmjolnircore.framework/Headers/ghmjolnircore.h
@@ -0,0 +1,18 @@
+//
+//  ghmjolnircore.h
+//  ghmjolnircore
+//
+//  Created by Uriel Rodriguez on 25/05/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ghmjolnircore.
+FOUNDATION_EXPORT double ghmjolnircoreVersionNumber;
+
+//! Project version string for ghmjolnircore.
+FOUNDATION_EXPORT const unsigned char ghmjolnircoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ghmjolnircore/PublicHeader.h>
+
+
